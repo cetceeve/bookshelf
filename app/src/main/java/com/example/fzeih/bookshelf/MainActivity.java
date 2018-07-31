0f573e6cfd3b766b0c76bf2d317e36efdcaaf2f4
kindOfCurly <marie.sautmann@gmx.de>
2018-07-31 15:23:59 +0200

    IMPELEMENT MainActivity: adapter, new booklist, intent

--- app/src/main/java/com/example/fzeih/bookshelf/MainActivity.java
+++ app/src/main/java/com/example/fzeih/bookshelf/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.fzeih.bookshelf;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mBooklistsDatabaseReference;
+
+    ArrayList<String> listnames = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
+
+    private String inputListname = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +41,57 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mBooklistsDatabaseReference = mFirebaseDatabase.getReference().child("booklists");
 
+        // TODO - Strings aus DB in Arraylist
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listnames);
+        ListView listListView = (ListView) findViewById(R.id.listview_listlist);
+        listListView.setAdapter(arrayAdapter);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setMessage("Enter new listname");
+                final EditText input = new EditText(getApplicationContext());
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        inputListname = input.getText().toString();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                // evlt. geht auch einfach builder.show()
+
+                listnames.add(inputListname);
+                arrayAdapter.notifyDataSetChanged();
+                // TODO - neue Liste in Firebase
+
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show(); - war vorgefertigt
             }
         });
+
+
+        listListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, BookListActivity.class);
+                // TODO - extras: Name der geklickten Liste mitgeben, über Firebase oder view?
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
