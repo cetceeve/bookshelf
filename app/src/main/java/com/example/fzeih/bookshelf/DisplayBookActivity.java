53ca80601c4b666888e496a74f759a65583940ee
oIksa <iska1@web.de>
2018-08-01 12:12:07 +0200

    DisplayBookActivity: create

--- app/src/main/java/com/example/fzeih/bookshelf/DisplayBookActivity.java
+++ app/src/main/java/com/example/fzeih/bookshelf/DisplayBookActivity.java
@@ -1,13 +1,32 @@
 package com.example.fzeih.bookshelf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayBookActivity extends AppCompatActivity {
+
+    private TextView titleTextView;
+    private TextView authorNameTextView;
+    private TextView isbnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_book);
+
+        Intent i = getIntent();
+        Bundle extra = i.getExtras();
+        Book currentBook = (Book)extra.get("book");
+
+        titleTextView = (TextView)findViewById(R.id.textView_title_book);
+        authorNameTextView = (TextView)findViewById(R.id.textView_authorName_book);
+        isbnTextView = (TextView)findViewById(R.id.textView_isbn_book);
+
+        titleTextView.setText(currentBook.getTitle());
+        authorNameTextView.setText(currentBook.getAuthorName());
+        isbnTextView.setText(currentBook.getIsbn());
+
     }
 }
