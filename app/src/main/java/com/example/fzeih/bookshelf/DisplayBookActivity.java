81375edc7834ab8e4a75101dac8c112f97bda21a
oIksa <iska1@web.de>
2018-08-01 14:25:42 +0200

    edit Button

--- app/src/main/java/com/example/fzeih/bookshelf/DisplayBookActivity.java
+++ app/src/main/java/com/example/fzeih/bookshelf/DisplayBookActivity.java
@@ -3,6 +3,8 @@ package com.example.fzeih.bookshelf;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayBookActivity extends AppCompatActivity {
@@ -10,6 +12,7 @@ public class DisplayBookActivity extends AppCompatActivity {
     private TextView titleTextView;
     private TextView authorNameTextView;
     private TextView isbnTextView;
+    private Button editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,19 @@ public class DisplayBookActivity extends AppCompatActivity {
         titleTextView = (TextView)findViewById(R.id.textView_title_book);
         authorNameTextView = (TextView)findViewById(R.id.textView_authorName_book);
         isbnTextView = (TextView)findViewById(R.id.textView_isbn_book);
+        editButton = (Button)findViewById(R.id.button_edit);
 
         titleTextView.setText(currentBook.getTitle());
         authorNameTextView.setText(currentBook.getAuthorName());
         isbnTextView.setText(currentBook.getIsbn());
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DisplayBookActivity.this,AddBookActivity.class);//vorerst
+                startActivity(intent);
+            }
+        });
 
     }
 }
