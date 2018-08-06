eb2a1c87c619bfba0d4949031178a2aa8d97a78f
Fabian Zeiher <fzeiher@gmail.com>
2018-08-06 12:38:05 +0200

    FEAT: implemented Network Framework Blueprint from Android Documentation

--- app/src/main/java/com/example/fzeih/bookshelf/IsbnSearchActivity.java
+++ app/src/main/java/com/example/fzeih/bookshelf/IsbnSearchActivity.java
@@ -1,16 +1,27 @@
 package com.example.fzeih.bookshelf;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class IsbnSearchActivity extends AppCompatActivity {
+public class IsbnSearchActivity extends AppCompatActivity implements DownloadCallback {
 
     private EditText isbnEditText;
     private Button searchButton;
     private ListView resultListView;
+
+    // Keep a reference to the NetworkFragment, which owns the AsyncTask object
+    // that is used to execute network ops.
+    private NetworkFragment mNetworkFragment;
+
+    // Boolean telling us whether a download is in progress, so we don't trigger overlapping
+    // downloads with consecutive button clicks.
+    private boolean mDownloading = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,7 @@ public class IsbnSearchActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("ISBN Search");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mNetworkFragment = NetworkFragment.getInstance(getFragmentManager(), "https://www.google.com");
 
         isbnEditText = (EditText) findViewById(R.id.edittext_isbn);
         searchButton = (Button) findViewById(R.id.button_searchByIsbn);
@@ -30,5 +42,54 @@ public class IsbnSearchActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void updateFromDownload(Object result) {
+        // Update your UI here based on result of download
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getActiveNetworkInfo();
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch (progressCode) {
+            // You can add UI behavior for progress updates here.
+            case Progress.ERROR:
+                // ...
+                break;
+            case Progress.CONNECT_SUCCESS:
+                // ...
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                // ...
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                // ...
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                // ...
+                break;
+        }
+    }
+
+    @Override
+    public void finishDownloading() {
+        mDownloading = false;
+        if (mNetworkFragment != null) {
+            mNetworkFragment.cancelDownload();
+        }
+    }
+
+    private void startDownload() {
+        if (!mDownloading && mNetworkFragment != null) {
+            // Execute the async download.
+            mNetworkFragment.startDownload();
+            mDownloading = true;
+        }
     }
 }
