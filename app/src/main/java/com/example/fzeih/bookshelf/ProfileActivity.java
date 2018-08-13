88b3b9b77596267805aa738b9be87374b09bcb3d
Fabian Zeiher <fzeiher@gmail.com>
2018-08-13 22:41:17 +0200

    FEAT: profile design improvements, shadow not working

--- app/src/main/java/com/example/fzeih/bookshelf/ProfileActivity.java
+++ app/src/main/java/com/example/fzeih/bookshelf/ProfileActivity.java
@@ -21,6 +21,7 @@ public class ProfileActivity extends AppCompatActivity {
     private ListView mAchievementListView;
     private TextView mNumOfBooksTextView;
     private TextView mNumOfReadBooksTextView;
+    private TextView mUserNameTextView;
     private ImageView mUserPhotoImageView;
 
     @Override
@@ -28,12 +29,13 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("User Profile");
         getSupportActionBar().setElevation(0);
 
         mUser = FirebaseAuth.getInstance().getCurrentUser();
-        setTitleBar();
 
         initViews();
+        setuserName();
         setUserPhoto();
 
         setAdapter();
@@ -45,9 +47,9 @@ public class ProfileActivity extends AppCompatActivity {
         return true;
     }
 
-    private void setTitleBar() {
+    private void setuserName() {
         if (mUser != null) {
-            getSupportActionBar().setTitle(mUser.getDisplayName());
+            mUserNameTextView.setText(mUser.getDisplayName());
         } else {
             Toast.makeText(ProfileActivity.this, "ERROR: user is not signed in", Toast.LENGTH_SHORT).show();
             finish();
@@ -65,6 +67,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void initViews() {
         mAchievementListView = findViewById(R.id.list_view_profile_achievements);
+        mUserNameTextView = findViewById(R.id.text_view_profile_user_name);
         mNumOfBooksTextView = findViewById(R.id.text_view_profile_amount_books);
         mNumOfReadBooksTextView = findViewById(R.id.text_view_profile_read_books);
         mUserPhotoImageView = findViewById(R.id.image_view_profile_user_photo);
@@ -78,13 +81,13 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void createAchievements() {
         achievements = new ArrayList<>();
-        achievements.add(new Achievement(this,10, R.string.achievement_10, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,25, R.string.achievement_25, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,50 ,R.string.achievement_50, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,100 ,R.string.achievement_100, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,250 ,R.string.achievement_250, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,500 ,R.string.achievement_500, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,1000 ,R.string.achievement_1000, R.mipmap.ic_launcher_round));
-        achievements.add(new Achievement(this,5000 ,R.string.achievement_5000, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 10, R.string.achievement_10, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 25, R.string.achievement_25, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 50, R.string.achievement_50, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 100, R.string.achievement_100, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 250, R.string.achievement_250, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 500, R.string.achievement_500, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 1000, R.string.achievement_1000, R.mipmap.ic_launcher_round));
+        achievements.add(new Achievement(this, 5000, R.string.achievement_5000, R.mipmap.ic_launcher_round));
     }
 }
