dcca6e6350c34564c5d5c78133d2605c7707c111
Fabian Zeiher <fzeiher@gmail.com>
2018-08-16 12:02:45 +0200

    FEAT: expand book object

--- app/src/main/java/com/example/fzeih/bookshelf/Book.java
+++ app/src/main/java/com/example/fzeih/bookshelf/Book.java
@@ -3,93 +3,168 @@ package com.example.fzeih.bookshelf;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class Book implements Parcelable {
 
     private String key;
-    private String authorName;
-    private String title;
-    private String isbn;
     private boolean read = false;
+    private String coverUrl;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String publisher;
+    private String publishedDate;
+    private int pages;
+    private String mainCategory;
+    private String bookDescription;
 
-    // TODO: deal with empty entries
     public Book() {
     }
 
-    public Book(String key, String authorName, String title, String isbn){
+    public Book(String key, boolean read, String coverUrl, String title, String author, String isbn,
+                String publisher, String publishedDate, int pages, String mainCategory, String bookDescription){
         this.key = key;
-        this.authorName = authorName;
-        this.title = title;
-        this.isbn = isbn;
-        this.read = false;
-    }
-
-    public Book(String key, String authorName, String title, String isbn, boolean read){
-        this.key = key;
-        this.authorName = authorName;
-        this.title = title;
-        this.isbn = isbn;
         this.read = read;
-    }
-
-    public void setRead(boolean read) {
-        this.read = read;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public void setTitle(String title) {
+        this.coverUrl = coverUrl;
         this.title = title;
-    }
-
-    public void setIsbn(String isbn) {
+        this.authorName = author;
         this.isbn = isbn;
+        this.publisher = publisher;
+        this.publishedDate = publishedDate;
+        this.pages = pages;
+        this.mainCategory = mainCategory;
+        this.bookDescription = bookDescription;
     }
 
     public String getKey() {
         return key;
     }
 
+    public boolean getRead() {
+        return read;
+    }
 
-    public String getAuthorName() {
-        return authorName;
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
     public String getIsbn() {
         return isbn;
     }
 
-    public boolean getRead() {
-        return read;
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    @Exclude
+    public String getPuplisherWithPublishedDate() {
+        return publisher + ", " + publishedDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getMainCategory() {
+        return mainCategory;
+    }
+
+    public void setMainCategory(String mainCategory) {
+        this.mainCategory = mainCategory;
+    }
+
+    public String getBookDescription() {
+        return bookDescription;
+    }
+
+    public void setBookDescription(String bookDescription) {
+        this.bookDescription = bookDescription;
+    }
+
+    @Exclude
     @Override
     public int describeContents() {
         return 0;
     }
 
+    @Exclude
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.key);
-        dest.writeString(this.authorName);
-        dest.writeString(this.title);
-        dest.writeString(this.isbn);
         dest.writeByte(this.read ? (byte) 1 : (byte) 0);
+        dest.writeString(this.coverUrl);
+        dest.writeString(this.title);
+        dest.writeString(this.authorName);
+        dest.writeString(this.isbn);
+        dest.writeString(this.publisher);
+        dest.writeString(this.publishedDate);
+        dest.writeInt(this.pages);
+        dest.writeString(this.mainCategory);
+        dest.writeString(this.bookDescription);
     }
 
     protected Book(Parcel in) {
         this.key = in.readString();
-        this.authorName = in.readString();
-        this.title = in.readString();
-        this.isbn = in.readString();
         this.read = in.readByte() != 0;
+        this.coverUrl = in.readString();
+        this.title = in.readString();
+        this.authorName = in.readString();
+        this.isbn = in.readString();
+        this.publisher = in.readString();
+        this.publishedDate = in.readString();
+        this.pages = in.readInt();
+        this.mainCategory = in.readString();
+        this.bookDescription = in.readString();
     }
 
+    @Exclude
     public static final Parcelable.Creator<Book> CREATOR = new Parcelable.Creator<Book>() {
         @Override
         public Book createFromParcel(Parcel source) {
